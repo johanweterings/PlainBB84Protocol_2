a2e78792c83246b06041736f154448c6598d2556
Johan Weterings <johan.weterings@gmail.com>
2021-08-31 18:54:07 +0200

    added last lines for the screen

--- Host/Program.cs
+++ Host/Program.cs
@@ -14,10 +14,19 @@ namespace Host
             var bobBits = "";
             var bobBases = "";
             var sentPhoton = "";
+            var comparedBases = "";
+            var sharedKey = "";
+            
+            int numberOfBits = 50;
+            if (args.Length > 1)
+            {
+                int.TryParse(args[0], out numberOfBits);
+            };
 
             var sim = new QuantumSimulator(throwOnReleasingQubitsNotInZeroState: true);
-            var result = await RunBB84Protocol.Run(sim, 50);
-            
+            var result = await RunBB84Protocol.Run(sim, numberOfBits);
+            bool match;
+
             foreach (var set in result)
             {
                 aliceBases += set.Item1 ? "X " : "Z ";
@@ -25,13 +34,24 @@ namespace Host
                 bobBases += set.Item3 ? "X " : "Z ";
                 bobBits += set.Item4 ? "1 " : "0 ";
                 sentPhoton += set.Item1 ? (set.Item2 ? "- " : "+ ") : (set.Item2 ? "1 " : "0 ");
+
+                match = set.Item1 == set.Item3;
+                comparedBases += match ? (set.Item1 ? "X " : "Z ") : "  ";
+                sharedKey += match ? (set.Item2 ? "1 " : "0 ") : string.Empty;
             }
 
+            Console.WriteLine("Quantum Transmission\n");
             Console.WriteLine(aliceBases);
             Console.WriteLine(aliceBits);
             Console.WriteLine(sentPhoton);
             Console.WriteLine(bobBases);
             Console.WriteLine(bobBits);
+
+            Console.WriteLine("\nPublic discussion\n");
+            Console.WriteLine(comparedBases);
+            Console.WriteLine($"Resulting shared key: {sharedKey}");
+
+
             Console.ReadLine();
         }
     }
